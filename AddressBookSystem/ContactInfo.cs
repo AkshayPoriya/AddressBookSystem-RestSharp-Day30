9bd0f47828c19408c25ee07bae96c7152b1c9bd0
AkshayPoriya <akshayporiya1234@gmail.com>
2020-11-13 07:23:14 +0530

    initial commit

---
+++ AddressBookSystem/ContactInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookSystem
+{
+    class ContactInfo
+    {
+        public string name { get; set; }
+        public string address { get; set; }
+        public string phoneNumber { get; set; }
+        public string email { get; set; }
+        public string contactType { get; set; }
+    }
+}
---
+++ MSTestForRestApiOperations/TestRestOperations.cs
@@ -0,0 +1,19 @@
+using AddressBookSystem;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+namespace MSTestForRestApiOperations
+{
+    [TestClass]
+    public class TestRestOperations
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
